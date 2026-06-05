119be2c6bcc79bc91eb0548a47e2e884eeb33f5a
fernandezmarti <163201630+fernandezmarti@users.noreply.github.com>
2026-06-05 15:00:10 -0300

    test

--- hardware.py
+++ hardware.py
@@ -76,13 +76,14 @@ class statusLED():
     
     def I2C_error(self):
         self.off()
-        for state, duration in PATTERN_3:
-            if state:
-                self.red.on()
-            else:
-                self.red.off()
+        while True:
+            for state, duration in PATTERN_3:
+                if state:
+                    self.red.on()
+                else:
+                    self.red.off()
 
-            time.sleep(duration)
+                time.sleep(duration)
 
 class Compressor():
     def __init__(self):
