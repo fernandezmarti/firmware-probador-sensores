1d646b59a9117953520fec856d66a28290ca5e9e
martin <m@gmail.com>
2026-09-10 13:48:40 -0300

    fin

--- plotter.py
+++ plotter.py
@@ -139,10 +139,10 @@ def boxplot_metrics(ax,folder, position):
 fig = plt.figure()
 plt.title("Flujo vs tiempo")
 #plot_N_tests(fig,"AAV1_T_a_SS_a_A22MM_SF_C/prueba 1 nueva matriz", sensirion=True,flux=True,all=True,color='red', show=False)
-plot_N_tests(fig,"AAV1_T_a_SS_a_A22MM_SF_C/29", sensirion=True,flux=True,all=True, color='green',show=False)
+plot_N_tests(fig,"Conexion larga sin codo/Validacion adaptador TPE/CON ADAPTADOR", sensirion=True,flux=True,all=True, color='green',show=False)
 #plot_N_tests(fig,"AAV1_T_a_SS_a_A22MM_SF_C/F20", sensirion=True,flux=True,all=True, color='brown',show=False)
 #plot_N_tests(fig,"AAV1_T_a_SS_a_A22MM_SF_C/sensor procesado de mas", sensirion=True,flux=True,all=True, color='yellow',show=False)
-plot_N_tests(fig,"Conexion larga sin codo/Pasan", sensirion=True,flux=True,all=True, color='red',show=False)
+plot_N_tests(fig,"Conexion larga sin codo/Validacion adaptador TPE/SIN ADAPTADOR", sensirion=True,flux=True,all=True, color='red',show=False)
 plt.grid(True)
 plt.show()
 """
